49ec4e917353a6c985a65c256340214f20bfde08
treva556 <marktreva92@gmail.com>
2022-11-27 18:43:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(action = "roller-skate"){
+    return `This Saturday, I want to ${action}!`
+}
+console.log(saturdayFun());
+
+
+function mondayWork(action ="go to the office"){
+    return `This Monday, I will ${action}.`
+}
+console.log(mondayWork());
+
+
+function wrapAdjective(symbol){
+    return function wrapper(adj)
+    {
+    return `You are ${symbol}${adj}${symbol}!`
+    }
+}
+console.log (wrapAdjective('||'));
